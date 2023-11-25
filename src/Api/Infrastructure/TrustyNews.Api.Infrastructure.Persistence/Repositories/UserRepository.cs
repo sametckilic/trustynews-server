b5b650afe7c1ea0395b753924d2018e2a8a01cba
sametdesktop <205541038@firat.edu.tr>
2023-11-25 21:11:47 +0300

    generic repo

---
+++ src/Api/Infrastructure/TrustyNews.Api.Infrastructure.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrustyNews.Api.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : GenericRepo
+    {
+    }
+}
